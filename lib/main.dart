7892132898839b683c8ed258200dc5bbd239ca0f
LucasFarias30 <Lucas.farias.branches@gmail.com>
2022-12-29 10:03:41 -0300

    myapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(PerguntaApp());
+void main() {
+  runApp(const PerguntaApp());
+}
 
 class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({super.key});
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é cor a sua cor favorita ?'
+          'Qual é seu animal favorito ?'
+    ];
+
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Perguntas'),
-        ),
-        body: Text('Olá flutter !!!'),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Pergunta'),
+            ),
+            body: Column(
+              children: [
+                Text(perguntas[0]),
+                const ElevatedButton(onPressed: null, child: Text('Resposta1')),
+                const ElevatedButton(onPressed: null, child: Text('Resposta2')),
+                const ElevatedButton(onPressed: null, child: Text('Resposta3')),
+              ],
+            )));
   }
 }
